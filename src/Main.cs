e097bb79eced967f0af6b49cc967517213a397f3
ankushk1729 <ankushrattan17@gmail.com>
2022-09-03 13:24:21 +0530

    feat: add create, use, update & delete recipe fns

--- src/Main.cs
+++ src/Main.cs
@@ -12,18 +12,34 @@ namespace GSMS {
 
             // Stock.decreaseQuantityOfItem("guava", 4);
             // Stock.updateQuantityOfItem("tomato", 7);
-            Stock.printQuantities();
+            // Stock.printQuantities();
 
-            Stock.addItem("lichi");
-            Stock.addItem("lemon", 5);
-            Stock.deleteItem("onion");
-            Stock.printQuantities();
+            // Stock.addItem("lichi");
+            // Stock.addItem("lemon", 5);
+            // Stock.deleteItem("onion");
 
-            var pastaRecipe = Recipe.getRecipe("pasta");
-            var allRecipes = Recipe.getAllRecipes();
+            // var pastaRecipe = Recipe.getRecipe("pasta");
+            // var allRecipes = Recipe.getAllRecipes();
             
+            // Recipe.printRecipe("maggi");
             // Recipe.printAllRecipes();
-            Recipe.printRecipe("maggi");
+            // var friedRiceItems = new Dictionary<string, int>(){
+            //     {"rice", 1},
+            //     {"onion", 1}
+            // };
+
+            // Recipe.createRecipe("friedRice", friedRiceItems);
+            Recipe.printAllRecipes();
+
+            // System.Console.WriteLine( Recipe.useRecipe("friedRice"));
+
+            // Recipe.deleteRecipe("pasta");
+
+            // System.Console.WriteLine(Recipe.updateRecipe("friedRice", new Dictionary<string, int>(){{"onion", 7}, {"water", 2}}));
+
+            Stock.printQuantities();
+
+
         }
     }
 }
--- src/recipe/Recipe.cs
+++ src/recipe/Recipe.cs
@@ -17,6 +17,12 @@ namespace GSMS
             return data.recipes[recipeName];
         }
 
+        public static bool checkContainsRecipe(string recipeName){
+            var data = Json.readFromJson();
+            
+            return data.recipes.ContainsKey(recipeName);
+        }
+
         // Loggers
         public static void printAllRecipes(){
             var allRecipes = Recipe.getAllRecipes();
@@ -31,6 +37,65 @@ namespace GSMS
             var recipe = getRecipe(recipeName);
             System.Console.WriteLine($"{recipeName} : ");
             Util.printTable(recipe);
+        }
+
+        // Updations
+
+        public static void createRecipe(string recipeName, Dictionary<string, int> items){
+            var data = Json.readFromJson();
+
+            data.recipes.Add(recipeName, items);
+
+            Json.writeToJson(data);
+        }
+
+        public static bool deleteRecipe(string recipeName){
+            var data = Json.readFromJson();
+            if(!Recipe.checkContainsRecipe(recipeName)){
+                return false;
+            }
+            data.recipes.Remove(recipeName);
+            Json.writeToJson(data);
+
+            return true;
+        }
+
+        public static bool updateRecipe(string recipeName, Dictionary<string, int> items){
+            if(!Recipe.checkContainsRecipe(recipeName)){
+                return false;
+            }
+
+            var data = Json.readFromJson();
+            foreach(KeyValuePair<string, int> item in items){
+                if(!data.recipes[recipeName].ContainsKey(item.Key)){
+                    data.recipes[recipeName].Add(item.Key, item.Value);
+                }
+                else {
+                    data.recipes[recipeName][item.Key] = item.Value;
+                }
+            }
+            Json.writeToJson(data);
+            return true;
+        }
+
+        public static bool useRecipe(string recipeName, int servings = 1){
+            var data = Json.readFromJson();
+
+            var recipe = data.recipes[recipeName];
+
+            foreach(KeyValuePair<string, int> item in recipe){
+                if(!Stock.checkContainsItem(item.Key) || data.stock[item.Key] < servings*item.Value){
+                    return false;
+                }
+            }
+
+            foreach(KeyValuePair<string, int> item in recipe){
+                data.stock[item.Key] -= servings*item.Value;
+            }
+
+            Json.writeToJson(data);
+
+            return true;
         }
     }
 }
